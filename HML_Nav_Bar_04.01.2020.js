99c39537e015416c89bceeeb3be57eb7c999454d
c-deems <70522956+c-deems@users.noreply.github.com>
2020-10-05 21:04:37 -0400

    Add files via upload

---
+++ HML_Nav_Bar_04.01.2020.js
@@ -0,0 +1,65 @@
+/* HML Navigation Last Updated: 04/01/20 */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function sideNavDrop() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+function sideNavDrop1() {
+  document.getElementById("myDropdown1").classList.toggle("show");
+}
+function sideNavDrop2() {
+  document.getElementById("myDropdown2").classList.toggle("show");
+}
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn1')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content1");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn2')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content2");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+function openNav() {
+  document.getElementById("mySidenav").style.width = "300px";
+}
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
